be615c60892627440dddea9d73e4e3ae9d4a8502
Daniel Boston <programmerdan@gmail.com>
2014-03-26 01:47:49 -0400

    Shaved a few using function wrapper for contains

--- AristotleNumber.java
+++ AristotleNumber.java
@@ -22,6 +22,7 @@ import java.util.*;
  * 1 and 19, with each number represented only once.
  */
 public class AristotleNumber {
+boolean c(Set<Integer>u,int z){return!u.contains(z);}
 	void win() {
 		int z= 0;
 		for (int a = 1; a <= 19; a++)
@@ -39,56 +40,56 @@ public class AristotleNumber {
 							used.add(a);used.add(b);used.add(c);
 							used.add(d);used.add(h);
 		for (int m = 1; m <= 19; m++)
-			if (!used.contains(m))
+			if (c(used,m))
 				for (int q = 1; q <= 19; q++)
-					if (q != m && !used.contains(q))
+					if (q != m && c(used,q))
 						if (h + m + q == 38) {
 							Set<Integer> used3 = new HashSet<Integer>(used);
 							used3.add(m); used3.add(q);
 		for (int r = 1; r <= 19; r++)
-			if (!used3.contains(r))
+			if (c(used3,r))
 				for (int s = 1; s <= 19; s++)
-					if (s != r && !used3.contains(s))
+					if (s != r && c(used3,s))
 						if (q + r + s == 38) {
 							Set<Integer> used4 = new HashSet<Integer>(used3);
 							used4.add(r); used4.add(s);
 		for (int p = 1; p <= 19; p++)
-			if (!used4.contains(p))
+			if (c(used4,p))
 				for (int l = 1; l <= 19; l++)
-					if (l != p && !used4.contains(l))
+					if (l != p && c(used4,l))
 						if (s + p + l == 38) {
 							Set<Integer> used5 = new HashSet<Integer>(used4);
 							used5.add(p); used5.add(l);
 		for (int g = 1; g <= 19; g++)
-			if (!used5.contains(g))
+			if (c(used5,g))
 				if (l + g + c == 38) {
 					// outerboards
 					Set<Integer> used6 = new HashSet<Integer>(used5);
 					used6.add(g);
 		for (int e = 1; e <= 19; e++)
-			if (!used6.contains(e))
+			if (c(used6,e))
 				for (int f = 1; f <= 19; f++)
-					if (f != e && !used6.contains(f))
+					if (f != e && c(used6,f))
 						if (d + e + f + g == 38) { 
 							Set<Integer> used7 = new HashSet<Integer>(used6);
 							used7.add(e); used7.add(f);
 		for (int i = 1; i <= 19; i++)
-			if (!used7.contains(i))
+			if (c(used7,i))
 				for (int n = 1; n <= 19; n++)
-					if (n != i && !used7.contains(n))
+					if (n != i && c(used7,n))
 						if (d + i + n + r == 38 && b + e + i + m == 38) {
 							Set<Integer> used8 = new HashSet<Integer>(used7);
 							used8.add(i); used8.add(n);
 		for (int o = 1; o <= 19; o++)
-			if (!used8.contains(o))
+			if (c(used8,o))
 				for (int k = 1; k <= 19; k++)
-					if (k != o && !used8.contains(k))
+					if (k != o && c(used8,k))
 						if (m + n + o + p == 38 && r + o + k + g == 38 &&
 							b + f + k + p == 38) {
 							Set<Integer> used9 = new HashSet<Integer>(used8);
 							used9.add(o); used9.add(k);
 		for (int j = 1; j <= 19; j++)
-			if (!used9.contains(j))
+			if (c(used9,j))
 				if (a + e + j + o + s == 38 && c + f + j + n + q == 38 &&
 					h + i + j + k + l == 38) {
 						z++;
